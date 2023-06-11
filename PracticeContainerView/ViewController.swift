9ff8e9764e48d3fc74967fb48ff05a2bff885ad3
motto3532 <gropmor74@gmail.com>
2023-06-11 22:10:16 +0900

    Initial Commit

---
+++ PracticeContainerView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeContainerView
+//
+//  Created by Atto Rari on 2023/06/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
