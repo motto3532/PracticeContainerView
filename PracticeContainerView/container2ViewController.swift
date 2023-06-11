6932c5aaf176ba9112ba08cbfeebdc7b84fba069
motto3532 <gropmor74@gmail.com>
2023-06-11 23:16:40 +0900

    containerViewとstoryboardReferenceでviewControllerを分けてみた

---
+++ PracticeContainerView/container2ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  container2ViewController.swift
+//  PracticeContainerView
+//
+//  Created by Atto Rari on 2023/06/11.
+//
+
+import UIKit
+
+class container2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PracticeContainerView/container3ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  container3ViewController.swift
+//  PracticeContainerView
+//
+//  Created by Atto Rari on 2023/06/11.
+//
+
+import UIKit
+
+class container3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
